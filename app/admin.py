fd66913dfe26ab7907ff74b6b8ff3672835f8725
Aamir Ashraf <aamirnasa97@gmail.com>
2023-02-08 16:54:40 +0530

    again some makemigrations and migrate

--- app/admin.py
+++ app/admin.py
@@ -1,9 +1,9 @@
 from django.contrib import admin
 from .models import User,Page
 # Register your models here.
-@admin.register(User)
-class UserAdmin(admin.ModelAdmin):
-    list_display=['id','user_name','password']
+# @admin.register(User)
+# class UserAdmin(admin.ModelAdmin):
+#     list_display=['user_name','password']
 @admin.register(Page)
 class PageAdmin(admin.ModelAdmin):
-    list_display=['id','page_name','page_catalog','publication_date']
+    list_display=['page_name','page_catalog','publication_date']
--- app/models.py
+++ app/models.py
@@ -1,13 +1,14 @@
 from django.db import models
+from django.contrib.auth.models import User
 
 # Create your models here.
-class User(models.Model):
-    user_name=models.CharField(max_length=30)
-    password=models.CharField(max_length=30)
+# class User(models.Model):
+#     user_name=models.CharField(max_length=30)
+#     password=models.CharField(max_length=30)
 
 class Page(models.Model):
     user=models.OneToOneField(User, on_delete=models.CASCADE)
     page_name=models.CharField(max_length=50)
     page_catalog=models.CharField(max_length=50)
-    publication_date=models.CharField(max_length=50)
-    
+    publication_date=models.DateField()
+
