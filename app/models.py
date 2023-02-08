162158ac13f0f11421ef018595df304ff6cbe340
Aamir Ashraf <aamirnasa97@gmail.com>
2023-02-08 17:46:33 +0530

    using protect

--- app/models.py
+++ app/models.py
@@ -6,9 +6,18 @@ from django.contrib.auth.models import User
 #     user_name=models.CharField(max_length=30)
 #     password=models.CharField(max_length=30)
 
+# class Page(models.Model):
+#     user=models.OneToOneField(User, on_delete=models.CASCADE,primary_key=True)
+#     page_name=models.CharField(max_length=50)
+#     page_catalog=models.CharField(max_length=50)
+#     publication_date=models.DateField()
+
+
+#lets use the protected concept in orm 
+#if there is any reference then the entity is not 
+#deleted
 class Page(models.Model):
-    user=models.OneToOneField(User, on_delete=models.CASCADE)
-    page_name=models.CharField(max_length=50)
+    user=models.OneToOneField(User,on_delete=models.PROTECT,primary_key=True)
+    page_name=models.CharField(max_length=30)
     page_catalog=models.CharField(max_length=50)
     publication_date=models.DateField()
-
