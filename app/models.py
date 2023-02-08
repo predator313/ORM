89e62c2322bed14305586d2834670bae41992840
Aamir Ashraf <aamirnasa97@gmail.com>
2023-02-08 16:01:40 +0530

    creating models

--- app/models.py
+++ app/models.py
@@ -1,3 +1,13 @@
 from django.db import models
 
 # Create your models here.
+class User(models.Model):
+    user_name=models.CharField(max_length=30)
+    password=models.CharField(max_length=30)
+
+class Page(models.Model):
+    user=models.OneToOneField(User, on_delete=models.CASCADE)
+    page_name=models.CharField(max_length=50)
+    page_catalog=models.CharField(max_length=50)
+    publication_date=models.CharField(max_length=50)
+    
